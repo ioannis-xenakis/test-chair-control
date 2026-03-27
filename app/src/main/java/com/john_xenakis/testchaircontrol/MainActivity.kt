136c65351af6578ecba79a5b944bef423acdf623
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2026-03-27 20:16:21 +0200

    Implement send receive text in main activity

--- app/src/main/java/com/john_xenakis/testchaircontrol/MainActivity.kt
+++ app/src/main/java/com/john_xenakis/testchaircontrol/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,37 +12,36 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.john_xenakis.testchaircontrol.ui.theme.TestChairControlTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            TestChairControlTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+    private lateinit var usb: UsbController
+
+    private val vm: ChairViewModel by viewModels {
+        object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                usb = UsbController(applicationContext)
+                return ChairViewModel(usb) as T
             }
         }
     }
-}
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        usb.register()
+        enableEdgeToEdge()
+        setContent {
+            TestChairControlTheme {
+                ChairControlScreen(vm)
+            }
+        }
+    }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TestChairControlTheme {
-        Greeting("Android")
+    override fun onDestroy() {
+        super.onDestroy()
+        usb.unregister()
     }
 }
