3bc0e61ab64b613476ec9b6a7a36de916e55bcb0
ioannis-xenakis <Xenakis.i.contact@gmail.com>
2026-03-29 14:27:30 +0300

    Replace usb controller with aoa in main activity
    Replace UsbController class with AoAController class in MainActivity.kt.

--- app/src/main/java/com/john_xenakis/testchaircontrol/MainActivity.kt
+++ app/src/main/java/com/john_xenakis/testchaircontrol/MainActivity.kt
@@ -1,36 +1,35 @@
 package com.john_xenakis.testchaircontrol
 
+import android.hardware.usb.UsbAccessory
+import android.hardware.usb.UsbManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.john_xenakis.testchaircontrol.ui.theme.TestChairControlTheme
 
 class MainActivity : ComponentActivity() {
 
-    private val usb by lazy { UsbController(applicationContext) }
+    private val aoa by lazy { AoAController(applicationContext) }
 
     private val vm: ChairViewModel by viewModels {
         object : ViewModelProvider.Factory {
             override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                return ChairViewModel(usb) as T
+                return ChairViewModel(aoa) as T
             }
         }
     }
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        usb.register()
+        // If the app was launched because an accessory was attached, open it.
+        val accessory = intent.getParcelableExtra<UsbAccessory>(UsbManager.EXTRA_ACCESSORY)
+        if (accessory != null) {
+            aoa.openFromIntent(accessory)
+        }
         enableEdgeToEdge()
         setContent {
             TestChairControlTheme {
@@ -41,6 +40,6 @@ class MainActivity : ComponentActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        usb.unregister()
+        aoa.close()
     }
 }
